90267811219119d1238ae87912e381e48b66c050
Bentley Cook <bentley.s.cook@gmail.com>
2015-04-23 15:19:40 -0400

    Initial commit

---
+++ oxford_comma/oxford_comma.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+register = template.Library()
+
+
+@register.tag
+def oxford_comma(parser, token):
+    # Given a list of items, properly comma and 'and' them together
+    try:
+        tag_name, list_of_items = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "{tag_name} tag requires exactly one argument".format(tag_name=token.contents.split()[0])
+        )
+    else:
+        return OxfordCommaNode(list_of_items)
+
+
+class OxfordCommaNode(template.Node):
+
+    def __init__(self, list_of_items):
+        self.list_of_items = template.Variable(list_of_items)
+
+    def render(self, context):
+        try:
+            list_of_items = self.list_of_items.resolve(context)
+        except template.VariableDoesNotExist:
+            return ''
+        else:
+            if len(list_of_items) == 2:
+                return "{first} and {second}".format(first=list_of_items[0], second=list_of_items[1])
+            else:
+                to_return = ''
+                for item in list_of_items:
+                    if list_of_items.index(item) == len(list_of_items) - 1:
+                        to_return += "and {item}".format(item)
+                    else:
+                        to_return += "{item}, ".format(item)
+                return to_return
